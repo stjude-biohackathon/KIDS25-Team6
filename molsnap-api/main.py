e277f6b4a0e94adc035a41b588837e15f7fb5160
Tyler Yang <tjyang2005@gmail.com>
2025-10-20 21:00:31 -0400

    update molsnap API with agent
    using OpenAI api, can hook up to something else if necessary

--- molsnap-api/main.py
+++ molsnap-api/main.py
@@ -7,6 +7,25 @@ from ML_model import prediction
 from fastapi.responses import JSONResponse
 import ast
 
+# for the agent
+# ref: https://lmstudio.ai/docs/app/api/endpoints/openai
+# ref: https://platform.openai.com/docs/guides/images-vision?api-mode=responses#analyze-images
+from openai import OpenAI
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+META_PROMPT = """
+You are a molecule classfication agent as part of a molecule recognition pipeline. 
+You will be given images of molecules from the user, and you MUST return a classification 
+from the following: "macrocycle", "natural product", or "neither". 
+You MUST respond with one of the classifications verbatim.
+"""
+# [IF USING o4-mini or some other reasoning model] 
+# You must think long and hard about what type of molecule is presented. 
+# If the molecule does not fit into any of the categories above, you MUST respond in one word, and that one word must be "generic".
+# You MUST respond with either one word or one of the category names verbatim.
+USER_PROMPT = """
+Classify the given image as a macrocycle or natural product. Please return 'macrocycle' 'natural product' or 'neither'
+"""
+
 app = FastAPI()
 
 origins = [
@@ -22,8 +41,52 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-async def get_prediction_results(uploaded_files, checkpoint_path='checkpoints/molnextr_best.pth'):
-    return prediction.predict_from_image_files(uploaded_files, checkpoint_path)
+async def get_agent_prediction_result(uploaded_file):
+    # call the agent to classify the image
+    completion = client.chat.completions.create(
+      model="gpt-4o", # use o1 or o4-mini if we want reasoning, will be slightly more expensive per token though.
+        
+      messages=[
+        {"role": "system", "content": META_PROMPT},
+        {"role": "user", "content": [
+            {"type": "text", "text": "Please classify the given molecule image into one of the following categories: "},
+            {
+                "type": "input_image",
+                "image_url": uploaded_file,
+            },
+        ]}
+      ],
+      response_format={"type": "text"},
+      # IF using reasoning model:
+      # reasoning_effort="medium", # can be "low", "medium", or "high" for o4-mini and other similar models in the family
+      
+      # IF using non-reasoning model (e.g., 4o or 4.1)
+      temperature=0.1, # even lower temperature is OK, but ChemEagle uses 0.1. Lower temperature = more determinism
+      max_completion_tokens=100,
+      top_p=1.0,
+    )
+    resp = completion.choices[0].message
+
+    if resp == "macrocycle":
+        # fine-tuned on macrocycles
+        return prediction.predict_from_image_files([uploaded_file], 'checkpoints/molnextr_macrocycle.pth')
+    elif resp == "natural product":
+        # fine-tuned on natural products
+        return prediction.predict_from_image_files([uploaded_file], 'checkpoints/molnextr_natprod.pth')
+    else:
+        assert resp == "neither" # hopefully the VLM follows instructions
+        # default model
+        return prediction.predict_from_image_files([uploaded_file], 'checkpoints/molnextr_best.pth')
+
+async def get_prediction_results(uploaded_files, checkpoint_path='checkpoints/molnextr_best.pth', use_agent=False):
+    if use_agent:
+        results = []
+        for file in uploaded_files:
+            results.append(get_agent_prediction_result(file))
+
+        return results 
+    else: 
+        return prediction.predict_from_image_files(uploaded_files, checkpoint_path)
 
 @app.get("/")
 def read_root():
@@ -31,11 +94,6 @@ def read_root():
 
 @app.post("/prediction-only",status_code=status.HTTP_200_OK)
 async def run_prediction(model: str = Form(...), images: str = Form(...)):
-    # call the agent to classify the image
-
-    
-
-
     # now we need to call the predition function from ML_model
     images_list = [os.path.join("decimer-api", img) for img in ast.literal_eval(images)]
     checkpoint_path = os.path.join("ML_Model/checkpoints", model)
